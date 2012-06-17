d9ebfae85b46861a07dc4e1608b68f6004484dcb
aliaksei.sanko@gmail.com <aliaksei.sanko@gmail.com@bdb80c27-877c-c390-78e8-174b791ddc99>
2012-06-17 15:27:20 +0000

    missing files added

---
+++ Worker.cpp
@@ -0,0 +1,32 @@
+#include "Worker.h"
+
+#include "solver.h"
+
+#include <QVector>
+#include <memory.h>
+
+Worker::Worker(const Data& data)
+{
+    memcpy(m_data, data, sizeof(Data));
+}
+
+
+Worker::~Worker()
+{
+}
+
+void Worker::run()
+{
+    QVector<unsigned char> result(100);
+
+    int solved = Solve(m_data, &result[0]);
+
+    if (solved < 0)
+    {
+        solved = 0;
+    }
+
+    result.erase(result.begin() + solved, result.end());
+
+    emit finished(result);
+}
